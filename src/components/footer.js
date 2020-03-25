d3a0ed78973cd0faae1ce9e627910b929ebd7546
Ashvin Moro <ashvinmoro@Ashvins-MacBook-Pro.local>
2020-03-24 23:13:15 -0400

    footer addition and nav tweaks

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,19 @@ import "./footer.css"
 const Footer = () => {
   return (
     <div className="footer-container">
-      <h1>Footer</h1>
+      <div className="left-side">
+        <h2>left side</h2>
+        <p>contact details?</p>
+      </div>
+      <div className="center">
+        <h2>center</h2>
+        <p>Logo maybe? Icon?</p>
+      </div>
+      <div className="right-side">
+        <h2>right side</h2>
+        <p>social medias?</p>
+        <p>check out my repo <a href="https://github.com/ashvin-moro/personal-website">here</a></p>
+      </div>
     </div>
   )
 }
